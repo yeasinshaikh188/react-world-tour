5b249bb908a30420fb4f31d495404ae1112299bb
yeasinshaikh188@gmail.com <yeasinshaikh188@gmail.com>
2024-08-27 13:50:47 +0600

    added event

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -6,6 +6,7 @@ import './countries.css'
 const Countries = () => {
       
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
   useEffect(() =>{
     fetch('https://restcountries.com/v3.1/all')
@@ -13,13 +14,28 @@ const Countries = () => {
     .then(data => setCountries(data));
   }, [])
 
+
+  const handleVisitedCountry = (country) =>{
+
+    console.log("add your visited country ");
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+   
+  }
+
     return (
         <div>
             <h3>Countries : {countries.length}</h3>
-          
+          <div>
+            <h4>Visited Countries: {visitedCountries.length} </h4>
+            {
+                visitedCountries.map(country =><li>{country.name.common}</li>)
+            }
+            
+          </div>
           <div className="countrie-container">
           {
-                countries.map(country => <Country country={country} key={country.cca3}></Country>)
+                countries.map(country => <Country handleVisitedCountry={handleVisitedCountry} country={country} key={country.cca3}></Country>)
             }
           </div>
         </div>
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './country.css'
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
     console.log(country)
     const {name, flags, population, area, cca3} = country;
 
@@ -9,14 +9,19 @@ const Country = ({country}) => {
     const handleVisited = () =>{
         setVisited(!visited);
     }
+    
+    const passWithParam = () => handleVisitedCountry(country);
+
+    
 
     return (
         <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
-            <h4>name: {name.common} </h4>
+            <h4 style={{color: visited ? 'yellow' : 'white'}}>name: {name.common} </h4>
             <img src={flags.png} alt="" />
             <p>Population: {population} </p>
             <p>Area: {area} </p>
             <p><small>code: {cca3} </small></p>
+            <button onClick={ ()=> handleVisitedCountry(country)}>Mark Visited</button>
             <button onClick={handleVisited}>{visited?"visited":'Going'} </button>
             {visited ?'I have visited this country': 'i want visit this country'}
         </div>
